34dee9c388848fed0776b8690a0255f8188a93e8
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-11-17 16:31:29 -0800

    Overview, Sources, and Libraries

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Project Overview: 
+I will create a Chrome Extension that packages a mario like game. 
+The game will have two main goals. The first goal is to complete all of the levels in the fastest time. 
+This is recorded through a game clock that is displayed. The second main goal of the game is to get the highest score possible. 
+Throghout the game differnt "Coins" can be gathered by the user colliding with that "Coin." 
+Users will be able record their scores by logging in.
+"""
+
+# Sources:
+    # https://www.youtube.com/watch?v=0n809nd4Zu4
+    # https://www.youtube.com/watch?v=uV4L-wcnK3Y
+    # https://support.google.com/chrome/a/answer/2714278?hl=en
+
+# Import Libraries
+import pygame as py
+import random
+
+# Import my built in libraries
+
+# global variables
+
+
