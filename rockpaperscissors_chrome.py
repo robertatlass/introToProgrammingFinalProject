69e4af1f00fb8b20fa51c438105dfcfb82d9c432
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-12-02 13:37:46 -0800

    Got rid of random library - made 2 player game

--- rockpaperscissors_chrome.py
+++ rockpaperscissors_chrome.py
@@ -5,43 +5,38 @@ Sources:
 Questions: What does the f and /n do that is the only difference between my first thoughts and seconds thoughts
 '''
 #Imports Random Library
-import random
-
-# Random Library
-
-
-
+# import random
 
 # Asks the player to pick their choice in the terminal
-player_choice = input("Please Pick from rock, paper, or scissors (case sensitive):")
+player1_choice = input("Player1: please Pick from rock, paper, or scissors (case sensitive):")
+player2_choice = input (" Player2: please Pick from rock, paper, or scissors (case sensitive):")
 # these are the choices the computer can pick from
 possible_choices = ["rock", "paper", "scissors"]
 # defines the function
 def myfunc(possible_choices):
 # Randomly picks from one of the possible_choices variable
-    computer_choice = random.choice(possible_choices)
-    print(f"\n The player chose {player_choice}, the computer chose {computer_choice}.\n")
+    print(f"\n The player chose {player1_choice}, the computer chose {player2_choice}.\n")
 # if the player choice and computer are the same this is what to print
-    if player_choice == computer_choice:
-        print(f"Both of you selected {player_choice}. That is lame! It's a tie!")
+    if player1_choice == player2_choice:
+        print(f"Both of you selected {player1_choice}. That is lame! It's a tie!")
 # All possible outcomes if the player chooses rock
-    elif player_choice == "rock":
-        if computer_choice == "scissors":
-            print("Rock crushes scissors! You win! Nice Job! :)")
+    elif player1_choice == "rock":
+        if player2_choice == "scissors":
+            print("Rock crushes scissors! Player 1 wins! Nice Job! :)")
         else:
-            print("Paper covers rock! Computer Wins! You are a loser!")
+            print("Paper covers rock! Player 2 wins! You are a loser!")
 # All possible outcomes if the player chooses paper           
-    elif player_choice == "paper":
-        if computer_choice == "rock":
-            print("Paper covers rock! You win! Nice Job! :)")
+    elif player1_choice == "paper":
+        if player2_choice == "rock":
+            print("Paper covers rock! Player 1 wins! Nice Job! :)")
         else:
-            print("Scissors cuts paper! Computer Wins! You are a loser!.")
+            print("Scissors cuts paper! player 2 wins! You are a loser!.")
 # All possible outcomes if the player chooses paper                 
-    elif player_choice == "scissors":
-        if computer_choice == "paper":
-            print("Scissors cuts paper! You win! Nice Job! :)")
+    elif player1_choice == "scissors":
+        if player2_choice == "paper":
+            print("Scissors cuts paper! player 1 wins! Nice Job! :)")
         else:
-            print("Rock crushes scissors! Computer Wins! You are a loser.")
+            print("Rock crushes scissors! player 2 wins! You are a loser.")
 myfunc(possible_choices)
 
 
