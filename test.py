b9167f93ee8fd3103760fcee6dc5f813ea17ac7d
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-12-06 17:30:10 -0800

    Notes and more transpiler checking

---
+++ test.py
@@ -0,0 +1,10 @@
+#  c:\users\robert.atlass23\appdata\local\programs\python\python310\lib\site-packages (from rapydscript)
+# rapydscript [options] <location of main file>
+print ("Hello World")
+
+
+
+DO THIS!!
+# find options for rapyscript compile
+
+
