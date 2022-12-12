7ad9f45f0c317e99d1321ff9f177817699efe598
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-12-11 18:15:08 -0800

    Update popup.py

--- Learning the extension/popup.py
+++ Learning the extension/popup.py
@@ -2,7 +2,6 @@
 Sources:
 1) https://realpython.com/python-rock-paper-scissors/ 
 2) https://codereview.stackexchange.com/questions/43024/use-if-else-elif-conditionals-to-write-a-basic-rock-paper-scissors-game
-Questions: What does the f and /n do that is the only difference between my first thoughts and seconds thoughts
 '''
 #Imports Random Library
 import random
@@ -39,46 +38,3 @@ def myfunc(possible_choices):
         else:
             print("Rock crushes scissors! Computer Wins! You are a loser.")
 myfunc(possible_choices)
-
-
-
-
-
-
-'''
-I AM NOT SURE WHY THE PRORGRAM GIVE INCORRECT STATEMENTS ON WHY THE PERSON OR COMPUTER WON THE GAME
-# Import Random Library
-import random
-# Create Play Again Option
-# Take Player Input
-player_choice = input("Please Pick from Rock, Paper, or Scissors (case sensitive): ") 
-player_choice == "Rock", 'Paper', 'Scissors'
-#define the list
-Computer_Choice = ['Rock', 'Paper', 'Scissors']
-# Defines The Function
-def myfunc(Computer_Choice): 
-   print("The Computer Picked " + random.choice(Computer_Choice))    
-#If Player and Computer Choices are the Same
-   if player_choice == Computer_Choice:
-      print("It is a Tie!")
-   # If player choice is paper - All Combinations
-   elif player_choice == "Paper":
-      if Computer_Choice == "Scissors":
-         print("Scissors Cut Paper, Computer Wins! YOU LOOSE HAHA!")
-      else: 
-         print("Paper Covers Rock, You Win!")
-   # If Player choice is Rock - All Combinations
-   elif player_choice == "Rock":
-      if Computer_Choice == "Scissors":
-         print("Rock Crushes Scissors, Computer Wins! YOU LOOSE HAHA!")
-   else: 
-         print("Paper Covers Rock, Computer Wins! YOU LOOSE HAHA!")
-   # If player choice is Scissors - All Combinations      
-   # elif player_choice == "Scissors":
-if Computer_Choice == "Paper":
-         print("Scissors Cut Paper, You Win!")   
-else:
-         print("Rock Crushes Scissors, Computer Wins! YOU LOOSE HAHA!")
-# Computer Picks Rock Paper or Scissors   
-myfunc(Computer_Choice)
-'''
