826f1cf0ae0fde0976f6eceab4d8842778002d92
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-12-14 10:22:05 -0800

    game alterations

--- Learning the extension/main.py
+++ Learning the extension/main.py
@@ -39,34 +39,45 @@ and convert it into javascript so that it is available for web use
 import random
 # ----------------------------------------- Rock Paper Scissors Code--------------------------------------------------------------
 # defines the function
+while True:
 # Asks the player to pick their choice in the terminal
-player_choice = input("Please Pick from rock, paper, or scissors (case sensitive):")
-# these are the choices the computer can pick from
-possible_choices = ["rock", "paper", "scissors"]
-# defines the function
-def myfunc(possible_choices):
-# Randomly picks from one of the possible_choices variable
+    player_choice = input("Please Pick from rock, paper, or scissors (case sensitive):")
+    # these are the choices the computer can pick from
+    possible_choices = ["rock", "paper", "scissors"]
+    # defines the function
+    # def myfunc(possible_choices):
+    # Randomly picks from one of the possible_choices variable
     computer_choice = random.choice(possible_choices)
     print(f"\n The player chose {player_choice}, the computer chose {computer_choice}.\n")
-# if the player choice and computer are the same this is what to print
+    # if the player choice and computer are the same this is what to print
     if player_choice == computer_choice:
         print(f"Both of you selected {player_choice}. That is lame! It's a tie!")
-# All possible outcomes if the player chooses rock
+    # All possible outcomes if the player chooses rock
     elif player_choice == "rock":
         if computer_choice == "scissors":
             print("Rock crushes scissors! You win! Nice Job! :)")
         else:
             print("Paper covers rock! Computer Wins! You are a loser!")
-# All possible outcomes if the player chooses paper           
+    # All possible outcomes if the player chooses paper           
     elif player_choice == "paper":
         if computer_choice == "rock":
             print("Paper covers rock! You win! Nice Job! :)")
         else:
             print("Scissors cuts paper! Computer Wins! You are a loser!.")
-# All possible outcomes if the player chooses paper                 
+    # All possible outcomes if the player chooses paper                 
     elif player_choice == "scissors":
         if computer_choice == "paper":
             print("Scissors cuts paper! You win! Nice Job! :)")
         else:
             print("Rock crushes scissors! Computer Wins! You are a loser.")
-myfunc(possible_choices)
+        print("Do you want to play again? (Y/N)")
+        ans = input().lower 
+        if ans == 'n':
+            break
+
+# myfunc(possible_choices)
+
+
+
+
+
