b5d0c0e7fef88d1465ad77e10de9a0b42f5b5990
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-12-15 21:41:32 -0800

    selenium possible test

--- Learning the extension/selenium_webdriver.py
+++ Learning the extension/selenium_webdriver.py
@@ -1,27 +1,37 @@
 
 ############## open website with selenium and webdriver ###################
-from time import sleep
+# from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.common.by import By
+
+# URL = 'https://herenow.com/results/#/races/20899/results'
+
+# chrome_driver = "Code\Projects\chromedriver.exe"
+# driver = webdriver.Chrome(chrome_driver)
+# driver.get(URL)
+# sleep(5)
+# # spans = driver.find_element(By.TAG_NAME, "span")
+# # print(type(spans))
+# searchresults = driver.find_elements(By.XPATH,"//span[contains(@class,'ng-binding')]")
+# print(searchresults[0].text)
+
+# datafound = []
+
+# for i in searchresults:
+#     if len(i.text) > 0:
+#         datafound.append(i.text)
+#     if len(datafound) > 45:
+#         break
+
+# print(datafound)
+
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-URL = 'https://herenow.com/results/#/races/20899/results'
+packed_extension_path = '/path/to/packed/extension.crx'
 
-chrome_driver = "Code\Projects\chromedriver.exe"
-driver = webdriver.Chrome(chrome_driver)
-driver.get(URL)
-sleep(5)
-# spans = driver.find_element(By.TAG_NAME, "span")
-# print(type(spans))
-searchresults = driver.find_elements(By.XPATH,"//span[contains(@class,'ng-binding')]")
-print(searchresults[0].text)
+options = Options()
+options.add_extension(packed_extension_path)
+driver = webdriver.Chrome(options=options)
 
-datafound = []
-
-for i in searchresults:
-    if len(i.text) > 0:
-        datafound.append(i.text)
-    if len(datafound) > 45:
-        break
-
-print(datafound)
