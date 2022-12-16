5fdc80ba2c2d4b212ffe4c8d21f416eaf029b59f
robertatlass <115025955+robertatlass@users.noreply.github.com>
2022-12-15 21:00:20 -0800

    Added comments and extra user inputs

--- Learning the extension/main.py
+++ Learning the extension/main.py
@@ -9,7 +9,7 @@ The Chrome extension uses the Brython transpiler to take the rock paper scirssor
 and convert it into javascript so that it is available for web use
 """
 
-# Disclaimer: The file history is located in the main.py file that is not in this folder. I copy and pasted all of this from my other main.py where I edited everythin
+# Disclaimer: The file GitHub commit history is located in the main.py file that is not in this folder. I copy and pasted all of this from my other main.py where I edited everything
 
 # ROBERT ATLASS
 
@@ -36,52 +36,47 @@ and convert it into javascript so that it is available for web use
     # https://bobbyhadz.com/blog/python-no-module-named-selenium#:~:text=The%20Python%20%22ModuleNotFoundError%3A%20No%20module,the%20pip%20install%20selenium%20command.
     # https://chromedriver.chromium.org/downloads
     # https://www.browserstack.com/guide/run-selenium-tests-using-selenium-chromedriver
+    # https://www.youtube.com/watch?v=zFZrkCIc2Oc
 
 # ---------------------------------------------------- Beginning of Code ---------------------------------------------------------
 
-# Import Libraries
+# import library
 import random
+
 # ----------------------------------------- Rock Paper Scissors Code--------------------------------------------------------------
 # defines the function
-while True:
 # Asks the player to pick their choice in the terminal
-    player_choice = input("Please Pick from rock, paper, or scissors (case sensitive):")
-    # these are the choices the computer can pick from
-    possible_choices = ["rock", "paper", "scissors"]
-    # defines the function
-    # def myfunc(possible_choices):
-    # Randomly picks from one of the possible_choices variable
+player_choice = input("Please Pick from rock, paper, or scissors (case sensitive):")
+# these are the choices the computer can pick from
+possible_choices = ["rock", "paper", "scissors"]
+# defines the function
+def myfunc(possible_choices):
+# Randomly picks from one of the possible_choices variable
     computer_choice = random.choice(possible_choices)
     print(f"\n The player chose {player_choice}, the computer chose {computer_choice}.\n")
-    # if the player choice and computer are the same this is what to print
+# if the player choice and computer are the same this is what to print
     if player_choice == computer_choice:
         print(f"Both of you selected {player_choice}. That is lame! It's a tie!")
-    # All possible outcomes if the player chooses rock
+        yourmid = input("admit you failed by typing... I AM WEAK AND CAN'T WIN!")
+# All possible outcomes if the player chooses rock
     elif player_choice == "rock":
         if computer_choice == "scissors":
             print("Rock crushes scissors! You win! Nice Job! :)")
         else:
             print("Paper covers rock! Computer Wins! You are a loser!")
-    # All possible outcomes if the player chooses paper           
+            yousuck1 = input("admit you failed by typing... I SUCK REALLY BADLY")
+# All possible outcomes if the player chooses paper           
     elif player_choice == "paper":
         if computer_choice == "rock":
             print("Paper covers rock! You win! Nice Job! :)")
         else:
             print("Scissors cuts paper! Computer Wins! You are a loser!.")
-    # All possible outcomes if the player chooses paper                 
+            yousuck2 = input("admit you failed by typing... I SUCK REALLY BADLY")
+# All possible outcomes if the player chooses paper                 
     elif player_choice == "scissors":
         if computer_choice == "paper":
             print("Scissors cuts paper! You win! Nice Job! :)")
         else:
             print("Rock crushes scissors! Computer Wins! You are a loser.")
-        print("Do you want to play again? (Y/N)")
-        ans = input().lower 
-        if ans == 'n':
-            break
-
-# myfunc(possible_choices)
-
-
-
-
-
+            yousuck3 = input("admit you failed by typing... I SUCK REALLY BADLY")
+myfunc(possible_choices)
